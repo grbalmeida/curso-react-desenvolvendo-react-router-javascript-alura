f45fec793b5df6f6957300f8b753c2d7eb981ffd
grbalmeida <g.r.almeida@live.com>
2023-10-12 20:00:50 -0300

    Utilizando NavLink

--- ola-mundo/src/componentes/MenuLink/index.js
+++ ola-mundo/src/componentes/MenuLink/index.js
@@ -1,16 +1,14 @@
-import { Link, useLocation } from "react-router-dom";
-import classNames from "classnames";
+import { NavLink } from "react-router-dom";
 
 import styles from "./MenuLink.module.css";
 
 export default function MenuLink({ children, to }) {
-    const localizacao = useLocation();
-
     return (
-        <Link className={classNames(styles.link, {
-            [styles.linkDestacado]: localizacao.pathname === to
-        })} to={to}>
+        <NavLink className={({ isActive }) => `
+            ${styles.link}
+            ${isActive ? styles.linkDestacado : ""}
+        `} to={to}>
             {children}
-        </Link>
+        </NavLink>
     )
 }
