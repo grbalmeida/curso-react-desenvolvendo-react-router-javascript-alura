90a5b275d70a0b18fc2af386864ff34819e82547
grbalmeida <g.r.almeida@live.com>
2023-10-12 19:10:20 -0300

    Criando o roteador

--- ola-mundo/src/App.js
+++ ola-mundo/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
  
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
@@ -6,6 +6,11 @@ import SobreMim from "./paginas/SobreMim";
 function App() {
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobre-mim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
     </BrowserRouter>
   );
 }
