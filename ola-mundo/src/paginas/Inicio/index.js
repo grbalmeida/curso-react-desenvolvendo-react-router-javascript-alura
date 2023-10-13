b22805cc39af18023ada96e99854a40e8cb9f140
grbalmeida <g.r.almeida@live.com>
2023-10-13 08:05:28 -0300

    Desenvolvendo Inicio

--- ola-mundo/src/paginas/Inicio/index.js
+++ ola-mundo/src/paginas/Inicio/index.js
@@ -1,11 +1,21 @@
 import Banner from "componentes/Banner";
+import Post from "componentes/Post";
+import styles from './Inicio.module.css';
+
+import posts from 'json/posts.json';
 
 export default function Inicio() {
     return (
         <main>
             <Banner />
 
-            <h1>Olá mundo!</h1>
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
